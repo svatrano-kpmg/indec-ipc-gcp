41a9852fdee0f1cf1120993dbb8bf2285d081e66
Sergio Vatrano <svatrano@gmail.com>
2025-11-21 09:28:58 -0300

    cambio en clour run y dockerfile para que levante el cr_downloader

--- scripts/02_deploy_cr_downloader.sh
+++ scripts/02_deploy_cr_downloader.sh
@@ -12,10 +12,11 @@ gcloud run deploy ${CR_DOWNLOADER} \
   --set-env-vars="PROJECT_ID=${PROJECT_ID},GCS_BUCKET=${GCS_RAW_BUCKET},PUB SUB_TOPIC=${TOPIC_RAW}"
   
 echo "--- Dando permisos al Scheduler (OIDC) para invocar ${CR_DOWNLOADER} ---"
+# no lo pude ejecutar por problemas con las credenciales del gcloud 
 gcloud run services add-iam-policy-binding ${CR_DOWNLOADER} \
   --region=${REGION} \
   --platform=managed \
   --member="serviceAccount:${SA_SCHEDULER}" \
   --role="roles/run.invoker"
 
-echo "--- Despliegue de Downloader completado ---"
+echo "--- Despliegue de Downloader completado ---"echo "--- Desplegando Cloud Run (Downloader): ${CR_DOWNLOADER} ---"
